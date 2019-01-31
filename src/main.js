b0c5d6eba89fd445b60cbb4c10f4424cb335cd41
Anuvrat Parashar <bhanuvrat@gmail.com>
2019-01-31 15:40:46 +0530

    configure async computed for use

--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import router from './router';
 import axios from 'axios';
 import store from './store';
 import './registerServiceWorker';
+import AsyncComputed from 'vue-async-computed';
 
-
+Vue.use(AsyncComputed);
 Vue.prototype.$http = axios;
 const token = localStorage.getItem('token');
 if (token) {
