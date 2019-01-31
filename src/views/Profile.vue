8951b3e18c1796758bb73ec2b01674923fcd8704
Anuvrat Parashar <bhanuvrat@gmail.com>
2019-01-31 17:39:27 +0530

    move conflicting css classes inside parent containers class for profile.vue
    Signed-off-by: Anuvrat Parashar <bhanuvrat@gmail.com>

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,24 +1,24 @@
-<template>
-  <div class="container">
+    <template>
+    <div class="profile-container">
     <h1>Profile</h1>
     <div class="form">
-      <form class="updateprofile" @submit.prevent="update_profile">
-        <div class="genderselect">
-          <multiselect v-model="selected_gender" placeholder="Gender" :options="gender_options">
-          </multiselect>
-        </div>
-        <div class="heightinput"><input v-model="profile_data.height_cm" type="text" class="text-input" placeholder="your height in cm"></div>
-        <div class="weightinput"><input  v-model="profile_data.weight_kg" type="text" class="text-input" placholder="your weight in kg"></div>
-        <div class="birthdateinput"><datepicker v-model="profile_data.date_of_birth"></datepicker></div>
-        <button type="submit" class="milife-button milife-button__fullsize">Update</button>
-      </form>
+    <form class="updateprofile" @submit.prevent="update_profile">
+    <div class="genderselect">
+    <multiselect v-model="selected_gender" placeholder="Gender" :options="gender_options">
+    </multiselect>
+    </div>
+    <div class="heightinput"><input v-model="profile_data.height_cm" type="text" class="text-input" placeholder="your height in cm"></div>
+    <div class="weightinput"><input  v-model="profile_data.weight_kg" type="text" class="text-input" placholder="your weight in kg"></div>
+    <div class="birthdateinput"><datepicker v-model="profile_data.date_of_birth"></datepicker></div>
+    <button type="submit" class="milife-button milife-button__fullsize">Update</button>
+    </form>
     </div>
     <div> Debug: {{profile_data}}</div>
-  </div>
-</template>
+    </div>
+    </template>
 
 
-<script>
+    <script>
 import axios from 'axios';
 import store from '@/store';2
 import Multiselect from 'vue-multiselect';
@@ -26,7 +26,7 @@ import Datepicker from 'vuejs-datepicker';
 
 export default {
     components: { Multiselect, Datepicker },
-
+    
     name: "UpdateProfile",
     data() {
         return {
@@ -41,9 +41,9 @@ export default {
                 gender: "",
             }
         }
-
+        
     },
-
+    
     created: function(){
         this.$store.dispatch('auth/fetch_profile')
             .then(() => console.log('fetched profile'))
@@ -52,77 +52,81 @@ export default {
     mounted: function(){
         console.log(this.$store.state.auth.user.email);
         this.profile_data.date_of_birth = this.$store.state.auth.user.date_of_birth;
-
+        
     },
-
+    
     methods: {
         update_profile(){
             this.$store.dispatch('auth/update_profile', this.profile_data)
                 .then(() => console.log('update_profile'))
                 .catch(err => console.log(err))
-
+            
         },
-
+        
     },
 }
 
 </script>
 
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+
 <style lang="scss">
-.genderinput {
-    width: 200px;
-}
-.birthdateinput {
-
-}
-.vdp-datepicker {
-    text-align: center;
-    padding-top: 20px;
-
-    color: #8AC53F;
-
-    input {
-        color: red;
-        border: none;
-        border-radius: 50px;
-        color: grey;
+.profile-container{
+    .genderinput {
+        width: 200px;
+    }
+    .birthdateinput {
+        
+    }
+    .vdp-datepicker {
+        text-align: center;
+        padding-top: 20px;
+        
+        color: #8AC53F;
+        
+        input {
+            color: red;
+            border: none;
+            border-radius: 50px;
+            color: grey;
+            height: 50px;
+            padding-left: 20px;
+            font-size: 15pt;
+            font-family: Monteserrat Regular;
+            width: 300px;
+            outline: none;
+            //padding-top: 20px;
+            margin-top: 20px;
+        }
+    }
+    
+    .genderselect {
         height: 50px;
+        width: 300px;
+        margin: auto;
+        .multiselect__tags {
+            border-radius: 50px;
+        }
+        
+    }
+    .multiselect__tags{
+        border-radius: 50px;
+        height: 50px;
+        width: 300px;
         padding-left: 20px;
         font-size: 15pt;
         font-family: Monteserrat Regular;
-        width: 300px;
-        outline: none;
-        //padding-top: 20px;
-        margin-top: 20px;
-    }
-}
-
-.genderselect {
-    height: 50px;
-    width: 300px;
-    margin: auto;
-    .multiselect__tags {
-        border-radius: 50px;
-    }
-
-}
-/*  .multiselect__tags{
-    border-radius: 50px;
-    height: 50px;
-    width: 300px;
-    padding-left: 20px;
-    font-size: 15pt;
-    font-family: Monteserrat Regular;
-
+        
     }
     .multiselect__select{
-    padding: 30px 8px;
+        padding: 30px 8px;
     }
-
-    }*/
-.vdp-datepicker__calendar {
-    margin: auto;
-    position: inherit;
+    
+    
+    .vdp-datepicker__calendar {
+        margin: auto;
+        position: inherit;
+    }
 }
+
 </style>
