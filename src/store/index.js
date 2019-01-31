732dfbd301a17b09bc62f34dbfad72ae65692e20
Anuvrat Parashar <bhanuvrat@gmail.com>
2019-01-31 15:43:47 +0530

    add store for user attributes

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,21 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import auth from './modules/auth'
-import VuexPersistence from 'vuex-persist'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import auth from './modules/auth';
+import user from './modules/user';
+import VuexPersistence from 'vuex-persist';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-const debug = process.env.NODE_ENV !== 'production'
+const debug = process.env.NODE_ENV !== 'production';
 
 const vuexLocal = new VuexPersistence({
     storage: window.localStorage
-})
+});
 
 export default new Vuex.Store({
     modules: {
         auth,
+        user,
     },
     strict: debug,
     //plugins: debug ? [createLogger()] : []
