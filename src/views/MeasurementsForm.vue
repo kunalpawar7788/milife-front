445eee9996327951d92691b8b0fdf3eaf6df7fb8
Anuvrat Parashar <bhanuvrat@gmail.com>
2019-02-18 12:49:12 +0530

    blank measurements input form view

---
+++ src/views/MeasurementsForm.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  Measurement input form
+</div>
+
+</template>
+
+<script>
+export default {
+    name: "MeasureemntsInput",
+    data() {
+        return {}
+    }
+  }
+</script>
+
+<style lang="scss">
+</style>
