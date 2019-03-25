555ee2489592b7c6d88f86a804d1574db479f4a7
Anuvrat Parashar <bhanuvrat@gmail.com>
2019-03-25 16:42:32 +0530

    add empty placeholder for  addDocument view

---
+++ src/views/userdocument/AddDocument.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="add-document">
+  
+</div>
+
+</template>
+
+<script>
+export default {
+    name: "AddDocument",
+    data() {
+    }
+}
+</script>
+
+
+<style>
+.add-document {
+}
+</style>
