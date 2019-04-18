9e9681b53ef871f42aa1c23cd8abd4476f60441f
Anuvrat Parashar <bhanuvrat@gmail.com>
2019-04-18 19:59:33 +0530

    add scaffold for upload csv functionality

---
+++ src/views/UploadCSVView.vue
@@ -0,0 +1,27 @@
+<template>
+<section class="upload-csv-container">
+  upload csv view
+  
+</section>
+</template>
+
+<script>
+export default {
+    name: "UploadCSVView",
+    data(){
+        return {
+            errors: "",
+        };
+    },
+    methods: {
+
+    },   
+
+}
+</script>
+
+<style lang="scss">
+section.upload-csv-container {
+    color: black;
+}
+</style>
