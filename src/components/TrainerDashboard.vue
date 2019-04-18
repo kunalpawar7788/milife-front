4d4d6a2f8c315ad1e9b5bbda1ac7205ec3fbd054
Anuvrat Parashar <bhanuvrat@gmail.com>
2019-04-18 20:21:27 +0530

    implement funmilayo's recommendations
    - add functionality to add a user
    - replace invite user with list of users pending invitation

--- src/components/TrainerDashboard.vue
+++ src/components/TrainerDashboard.vue
@@ -6,7 +6,7 @@
       <img class="image" id="icon-active" src="@/assets/images/activated-users.svg"/>
       <div class="count" id="count-active">{{status_d.active}}</div>
       <div class="label" id="label-active">ACTIVE <br/> USERS</div>
-      
+
       <img class="image" id="image-waiting" src="@/assets/images/invitation-waiting.svg"/>
       <div class="count" id="count-waiting">{{status_d.waiting}}</div>
       <div class="label" id="label-waiting">WATING <br/> CONFIRMATION</div>
@@ -16,26 +16,30 @@
     </div>
   </div>
   </section>
-  <div id="td-inviteuser">
+  <div id="td-adduser" v-on:click="goto_add_user">
     <span> </span>
-    <span class="cta"> <router-link :to="{ name: 'invite'}">INVITE A USER </router-link></span>
+    <span class="cta">ADD USER</span>
     <span class="arrow"> > </span>
   </div>
-  
+  <div id="td-inviteuser" v-on:click="goto_invite_user">
+    <span> </span>
+    <span class="cta"> SEND INVITATION</span>
+    <span class="arrow"> > </span>
+  </div>
+
   <div id="td-search">
     <router-link :to="{ name: 'user-list'}">
       <p>Search A User</p>
       <input class="text-input" required v-model="email" type="email" placeholder="start typing to search"/>
     </router-link>
   </div>
-  <div id="td-upload-csv">
+  <div id="td-upload-csv" v-on:click="goto_upload_csv">
     <span> </span>
-    <span class="cta"> <router-link :to="{ name: 'invite'}"> UPLOAD CSV</router-link></span>
+    <span class="cta">UPLOAD CSV</span>
     <span class="arrow"> + </span>
-    
   </div>
-  
-</div>  
+
+</div>
 </template>
 
 <script>
@@ -53,6 +57,15 @@ export default {
     components: {
     },
     methods: {
+        goto_upload_csv: function(){
+            this.$router.push({name: 'upload-csv'});
+        },
+        goto_add_user: function(){
+            this.$router.push({name: 'add-user'});
+        },
+        goto_invite_user: function(){
+            this.$router.push({name: 'pending-invitations'});
+        },
         invitation_status_count: function(){
             const url = process.env.VUE_APP_BASE_URL+'/api/counts/email_verification_status';
             axios.defaults.headers.common['Authorization'] = "Token " + localStorage.getItem('token');
@@ -65,10 +78,10 @@ export default {
                 })
                 .catch(err => {
                     err.response.data['errors'].forEach((element, index, array) =>{
-                        errors[element['field']] = element['message']   
+                        errors[element['field']] = element['message']
                     });
                     this.errors = errors;
-                });            
+                });
         }
     },
     mounted() {
@@ -80,81 +93,94 @@ export default {
 <style lang="scss">
 
 .trainer-dashboard {
+    * {
+        color: white;
+    }
     section.slab1{
         background-color: $milife-blue;
         border-radius: 20px;
         padding: 10px;
     }
-}
-/* .slab1 { */
-/*     border: 1px solid white; */
-/*     padding: 10px; */
-/* } */
-.usercounts {
-    display: grid;
-    grid-template-columns: 2fr 1fr 5fr;
-    background-color: $milife-blue;
-    padding-top: 20px;
-    padding-bottom: 20px;
-    border-radius: 20px;
-    height: 20%;
-}
-.usercounts>.count{
-    font-size: 30pt;
-    justify-self: center;
-    align-self: center;
-    padding: 5px 2px;
-    font-weight: bold;
-    color: white;
-}
-.usercounts>.image {
-    justify-self: end;
-    align-self: center;
-    padding: 5px 2px;
-}
-.usercounts>.label {
-    justify-self: start;
-    text-align: left;
-    align-self: center;
-    font-size: 10pt;
-    color: $milife-magenta;
-    padding: 5px 2px;
-}
 
-#td-inviteuser {
-    font-size: 20pt;
-    text-align: left;
-    background-color: $milife-green;
-    border-radius: 10px;
-    display: grid;
-    grid-template-columns: 1fr 10fr 1fr;
-    padding: 10px;
-    margin: 20px 2px;
-}
+    /* .slab1 { */
+    /*     border: 1px solid white; */
+    /*     padding: 10px; */
+    /* } */
+    .usercounts {
+        display: grid;
+        grid-template-columns: 2fr 1fr 5fr;
+        background-color: $milife-blue;
+        padding-top: 20px;
+        padding-bottom: 20px;
+        border-radius: 20px;
+        height: 20%;
+    }
+    .usercounts>.count{
+        font-size: 30pt;
+        justify-self: center;
+        align-self: center;
+        padding: 5px 2px;
+        font-weight: bold;
+    }
+    .usercounts>.image {
+        justify-self: end;
+        align-self: center;
+        padding: 5px 2px;
+    }
+    .usercounts>.label {
+        justify-self: start;
+        text-align: left;
+        align-self: center;
+        font-size: 10pt;
+        color: $milife-magenta;
+        padding: 5px 2px;
+    }
+    #td-adduser {
+        font-size: 20pt;
+        text-align: left;
+        background-color: $milife-green;
+        border-radius: 10px;
+        display: grid;
+        grid-template-columns: 1fr 10fr 1fr;
+        padding: 10px;
+        margin: 20px 2px;
+    }
 
-#td-upload-csv {
-    font-size: 20pt;
-    text-align: left;
-    background-color: $milife-magenta;
-    border-radius: 10px;
-    display: grid;
-    grid-template-columns: 1fr 10fr 1fr;
-    padding: 10px;
-    margin: 20px 2px;
-}
+    #td-inviteuser {
+        font-size: 20pt;
+        text-align: left;
+        background-color: $milife-green;
+        border-radius: 10px;
+        display: grid;
+        grid-template-columns: 1fr 10fr 1fr;
+        padding: 10px;
+        margin: 20px 2px;
 
-#td-search {
-    height: 20%;
-    background-color: $milife-grey;
-    border-radius:10px;
-    padding: 10px;
-    font-size: 20pt;
-}
-#td-search>a>input{
-    margin-top: 5px;
-}
-.user-list-link{
-    margin: 10px 1px;
-}
+    }
 
+    #td-upload-csv {
+        font-size: 20pt;
+        text-align: left;
+        background-color: $milife-magenta;
+        border-radius: 10px;
+        display: grid;
+        grid-template-columns: 1fr 10fr 1fr;
+        padding: 10px;
+        margin: 20px 2px;
+    }
+
+    #td-search {
+        height: 20%;
+        background-color: $milife-grey;
+        border-radius:10px;
+        padding: 10px;
+        font-size: 20pt;
+    }
+    #td-search>a>input{
+        margin-top: 5px;
+    }
+    .user-list-link{
+        margin: 10px 1px;
+    }
+}
 </style>
