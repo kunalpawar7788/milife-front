9a7e9ec935a1411dd51c8dd89fc833aa1fd5b2d0
Anuvrat Parashar <bhanuvrat@gmail.com>
2019-04-18 20:24:40 +0530

    add new views to the route

--- src/router.js
+++ src/router.js
@@ -15,17 +15,29 @@ import Unauthorized from '@/views/Unauthorized.vue';
 import ResetPassword from "@/views/ResetPassword.vue";
 import VerifyUserEmail from './views/VerifyUserEmail.vue';
 import Profile from '@/views/Profile.vue';
-import UserView from '@/views/UserView.vue';
-import UserList from '@/views/UserList.vue';
-import UserDetail from '@/views/UserDetail.vue';
+import UserView from '@/views/users/UserView.vue';
+import UserList from '@/views/users/UserList.vue';
+import UserDetail from '@/views/users/UserDetail.vue';
 import Test from '@/views/Test.vue';
 import GraphTest from '@/views/GraphTest.vue';
 import ComposeMessage from '@/views/ComposeMessage.vue';
-import ManageUser from '@/views/ManageUser.vue';
+import ManageUser from '@/views/users/ManageUser.vue';
 import DocumentList from '@/views/userdocument/DocumentList.vue';
 import AddDocument from '@/views/userdocument/AddDocument.vue';
 import ProgressChart from '@/views/ProgressChart.vue';
 import MeasurementsForm from '@/views/MeasurementsForm';
+import TargetWeightsView from '@/views/TargetWeightsView.vue';
+
+import ProgrammeList from '@/views/programme/ProgrammeList.vue';
+import ProgrammeDetail from '@/views/programme/ProgrammeDetail.vue';
+import ProgrammeAddEdit from '@/views/programme/ProgrammeAddEdit.vue';
+import ProgrammeEditSessions from '@/views/programme/ProgrammeEditSessions.vue';
+import HolidayAdd from '@/views/programme/HolidayAdd.vue';
+import BankSessionAdd from '@/views/programme/BankSessionAdd.vue';
+import LogWeightView from '@/views/LogWeightView.vue';
+import AddUserView from '@/views/users/AddUserView.vue';
+import UploadCSVView from "@/views/UploadCSVView.vue";
+import PendingUserList from "@/views/users/PendingUserList.vue";
 
 Vue.use(Router);
 
@@ -41,6 +53,8 @@ const router = new Router({
           meta: {requiresAuth: true},
       },
       { path: '/login', component: Login, name: 'login', meta: {requiresAnon: true}},
+      { path: '/add-user', component: AddUserView, name: 'add-user', meta: {requiresAdmin: true}},
+      { path: '/upload-csv', component: UploadCSVView, name: 'upload-csv', meta: {requiresAdmin: true}},
       { path: '/sign-up', component: Registration, name: 'sign-up', meta: {requiresAnon: true}},
       //{ path: '/verify-user-email', component: VerifyUserEmail, name: "verification-mail-sent"},
       { path: '/verify-user-email/:token', component: VerifyUserEmail, name: "verify-email-token"},
@@ -51,8 +65,10 @@ const router = new Router({
       { path: '/reset-password/:token', component: ResetPassword, name: "reset-password", meta: {requiresAnon: true}},
 
       { path: '/profile', component: Profile, name: "update-profile", meta: {requiresAuth: true}},
+      { path: '/log-weight', component: LogWeightView, name: "log-weight", meta: {requiresAuth: true}},
       { path: '/test', component: Test, name: "testview", meta: {requiresAuth: true}},
       { path: '/graphtest', component: GraphTest, name: "graphtestview", meta: {requiresAuth: false}},
+      { path: '/pending-invitations', component: PendingUserList, name: "pending-invitations", meta: {requiresAdmin: true}},
       { path: '/invite', component: Invitation, name: "invite", meta: {requiresAdmin: true}},
       { path: '/users', component: UserList, name: "user-list", meta: {requiresAdmin: true}},
       { path: '/users/:pk',
@@ -61,12 +77,29 @@ const router = new Router({
         children:[
             {path: '', component: UserDetail, name: "user-detail"},
             { path: 'manage', component: ManageUser, name: "user-manage", meta: {requiresAdmin: true}},
+            { path: 'target-weights', component: TargetWeightsView, name: "target-weights", meta: {requiresAdmin: true}},
             { path: 'compose-message', component: ComposeMessage, name: "user-message", meta: {requiresAdmin: true}},
             { path: 'documents', component: DocumentList, name: "user-documents", meta: {requiresAdmin: true}},
             { path: 'documents/:doc_pk/edit', component: AddDocument, name: "user-document-edit", meta: {requiresAdmin: true}},
             { path: 'documents/add', component: AddDocument, name: "user-document-add", meta: {requiresAdmin: true}},
             { path: 'progress-chart', component: ProgressChart, name: "progress-chart", meta: {requiresAdmin: true}},
             { path: 'add-checkin', component: MeasurementsForm , name: "add-checkin", meta: {requiresAdmin: true}},
+            { path: 'programmes', component: ProgrammeList, name: "programme-list", meta: {requiresAdmin: true}},
+            { path: 'programmes/add', component: ProgrammeAddEdit, name: "programme-add", meta: {requiresAdmin: true}},
+            { path: 'log-weight', component: LogWeightView, name: "admin-log-weight", meta: {requiresAuth: true}},
+            { path: 'programmes/:programme_pk',
+              meta: {requiresAdmin: true},
+              children: [
+                  { path: '', component: ProgrammeDetail, name: "programme-detail", meta: {requiresAdmin: true}},
+                  { path: 'edit', component: ProgrammeAddEdit, name: "programme-edit", meta: {requiresAdmin: true}},
+                  { path: 'edit-sessions', component: ProgrammeEditSessions, name: "programme-edit-sessions", meta: {requiresAdmin: true}},
+                  { path: 'holidays/add', component: HolidayAdd, name: "programme-holiday-add", meta: {requiresAdmin: true}},
+                  { path: 'banksessions/add', component: BankSessionAdd, name: "programme-banksession-add", meta: {requiresAdmin: true}},
+
+
+              ]
+            },
+
 
         ]
       },
