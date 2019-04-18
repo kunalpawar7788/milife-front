4c99be64ee0578c9ed69957b8bc03efc06859c17
Anuvrat Parashar <bhanuvrat@gmail.com>
2019-04-18 19:57:23 +0530

    add reusable component to display selected user at the top of user related views.

---
+++ src/components/SelectedUserDisplay.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="disabled-input-text">
+  <input
+    type="text"
+    class="text-input"
+    :value="full_name"
+    disabled=true
+    >
+    </input>
+</div>
+</template>
+
+<script>
+export default{
+    name: "SelectedUserDisplay",
+    props: ['fobj_user',],
+    computed: {
+        user() {return this.fobj_user;},
+        full_name() {
+            return this.user.first_name + " " + this.user.last_name;
+        },
+    },
+}
+</script>
+
+<style lang="scss">
+.disabled-input-text {
+    input{
+        background-color: lighten(grey, 40%);
+        text-align: center;
+        padding-left: 0;
+
+    }
+}
+
+</style>
