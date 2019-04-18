6e6ad1f0ff8330e1fbe02d83a479913940d0939e
Anuvrat Parashar <bhanuvrat@gmail.com>
2019-04-18 19:59:52 +0530

    add scaffold view to show users who haven't verified their emails

---
+++ src/views/users/PendingUserList.vue
@@ -0,0 +1,12 @@
+<template>
+<FilteredUserList email_verified="False"></FilteredUserList>
+</template>
+
+<script>
+import FilteredUserList from "@/components/FilteredUserList";
+export default {
+    name: "PendingUserList",
+    components: {FilteredUserList,},
+    
+}
+</script>
