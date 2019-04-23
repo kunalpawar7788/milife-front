d7c6b259289f130074cf2f5f405493a55f55d864
Anuvrat Parashar <bhanuvrat@gmail.com>
2019-04-23 16:03:01 +0530

    add router-view for programme

---
+++ src/views/programme/ProgrammeView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+
+<router-view v-bind:fobj_user="fobj_user"> </router-view>
+</div>
+</template>
+
+<script>
+export default {
+    name: "ProgrammeView",
+    props: ['fobj_user'],
+
+    data () {
+        return {
+            pk:  this.$route.params.pk,
+        }
+    },
+    methods: {
+        fetch_user: function(pk){
+            const url = process.env.VUE_APP_BASE_URL+'/api/users/' + this.pk;
+            this.$http.defaults.headers.common['Authorization'] = "Token " + localStorage.getItem('token');
+
+            return new Promise((resolve, reject) => {
+                this.$http({url: url, params:this.params, method: 'GET'})
+                    .then(resp => {
+                        resolve(resp.data);
+                    }).catch(err => {
+                        reject(err);
+                    });
+            });
+        },
+    },
+
+    asyncComputed: {
+        user:{
+            get () {
+                return  this.fetch_user();
+            },
+            default: {}
+        },
+    },
+    mounted() {
+        this.$store.dispatch("theme/set_theme_blue");
+    },
+
+
+}
+</script>
