f1b0b2f33662518a043fa1e8d9340506cc2cd175
Anuvrat Parashar <bhanuvrat@gmail.com>
2019-05-30 12:23:09 +0530

    add empty scaffold for view document

---
+++ src/views/userdocument/ViewDocument.vue
@@ -0,0 +1,22 @@
+<template>
+  <pdf :src="source" :page="1">
+    <template slot="loading">
+      loading content here...
+    </template>
+  </pdf>
+</template>
+
+<script>
+import pdf from 'pdfvuer'
+
+export default {
+data() {
+return {
+source: "/home/anuvrat/Documents/clients/lff-fitness/lff-proposal.pdf",
+}
+},
+  components: {
+    pdf
+  }
+}
+</script>
