43b166a81aa9772b5f539308eb0b475c3be7f346
Anuvrat Parashar <bhanuvrat@gmail.com>
2019-06-04 16:40:09 +0530

    feature: upload accuniq csv data to populate checkin fields

--- src/views/UploadCSVView.vue
+++ src/views/UploadCSVView.vue
@@ -1,7 +1,25 @@
 <template>
 <section class="upload-csv-container">
-  upload csv view
-  
+
+  <p>{{csvfile}}</p>
+
+  <input
+    class="csvfile"
+    type="file"
+    ref="csvfile"
+    id="csvfile"
+    value="csvfile"
+    v-on:change="handleFileSelect()"
+    >  </input>
+
+  <div class="button" v-on:click="upload_csv"> Upload selected file </div>
+
+
+  {{status}}
+
+
+
+
 </section>
 </template>
 
@@ -11,11 +29,50 @@ export default {
     data(){
         return {
             errors: "",
-        };
+            csvfile: '',
+            status: '',
+            upload_percentage: 0,
+        }
     },
-    methods: {
+    computed: {
+        upsert_url: function(){
+            return process.env.VUE_APP_BASE_URL+'/api/accuniq-data/';
+        },
+    },
 
-    },   
+    methods: {
+        handleFileSelect: function(){
+            this.csvfile = this.$refs.csvfile.files[0]
+        },
+
+        upload_csv: function(){
+            this.upload_percentage = 0
+            let formData = new FormData()
+            formData.append('csvfile', this.csvfile)
+
+            return new Promise((resolve, reject) => {
+                this.$http({
+                    url: this.upsert_url,
+                    data: formData,
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    },
+                    onUploadProgress: function( progressEvent ) {
+                        this.uploadPercentage = parseInt( Math.round( ( progressEvent.loaded * 100 ) / progressEvent.total ) )
+                    }.bind(this)
+                })
+                    .then(resp => {
+                        resolve(resp)
+                        this.$router.go('-1')
+                    }).catch(err => {
+                        console.log('failure', err)
+                        reject(err)
+                    })
+            })
+        },
+
+    },
 
 }
 </script>
