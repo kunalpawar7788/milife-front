1dc9a61d7eff38813e3518f052d6261748103262
Anuvrat Parashar <bhanuvrat@gmail.com>
2019-06-15 18:08:51 +0530

    add scaffold for weight summary card

---
+++ src/components/weight/WeightSummaryCard.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="weight-summary-card">
+  
+</div>
+</template>
+
+<script>
+export default {
+    name: "WeightSummaryCard",
+    data() {
+        return {
+        };
+    },
+    props: {},
+    computed: {},
+    methods: {},
+}
+</script>
+
+<style lang="scss">
+.weight-summary-card {
+    
+}
+</style>
