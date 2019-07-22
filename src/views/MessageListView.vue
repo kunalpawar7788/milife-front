4a5719d65b81707e4e8e8342733e7721092fb7eb
Anuvrat Parashar <bhanuvrat@gmail.com>
2019-07-22 06:16:26 +0530

    add scaffold for message-list and details view

---
+++ src/views/MessageListView.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="messages-list-view">
+  <h1>Messages List</h1>  
+</div>
+</template>
+
+<script>
+export default {
+    name: "MessageListView",
+    props: [],
+    data() {
+        return {
+        }
+    },
+    components: {
+
+    },
+    methods: {
+
+    },
+
+    computed: {
+    },
+
+    created() {
+    },
+    mounted() {
+    },
+}
+</script>
+
+<style lang="scss">
+.messages-list-view {
+
+}
+</style>
---
+++ src/views/MyDetailsView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="my-details-view">
+    <h1>Detail View</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "MyDetailsView",
+
+}
+</script>
+
+
+<style lang="scss">
+  
+</style>
