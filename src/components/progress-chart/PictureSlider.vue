f5bb088fd5778a682566ddf0ccf09f64bdac824a
Parth Sharma <parth261297@gmail.com>
2020-03-06 15:11:37 +0530

    improve formatting

--- src/components/progress-chart/PictureSlider.vue
+++ src/components/progress-chart/PictureSlider.vue
@@ -1,78 +1,61 @@
 <template>
-<div class="picture-slider">
-  <template v-if="show_front">
-    <div class="picture">
-      <img :src="pictures.original_front_profile"
-           alt="NA" />
-    </div>
-    <div class="picture">
-      <img :src="pictures.current_front_profile"
-           alt="NA"
-           />
-    </div>
-    <div class="arrow-div" v-on:click="show_front=false">
-      <div class="arr-right">
+  <div class="picture-slider">
+    <template v-if="show_front">
+      <div class="picture">
+        <img :src="pictures.original_front_profile" alt="NA" />
       </div>
-    </div>
-  </template>
-  <template v-else>
-    <div class="arrow-div" v-on:click="show_front=true">
-      <div class="arr-left">
+      <div class="picture">
+        <img :src="pictures.current_front_profile" alt="NA" />
       </div>
-    </div>
-    <div class="picture">
-      <img :src="pictures.original_side_profile"
-           alt="NA"
-           />
-
-    </div>
-    <div class="picture">
-      <img :src="pictures.current_side_profile"
-           alt="NA"
-           />
-    </div>
-  </template>
-</div>
+      <div class="arrow-div" v-on:click="show_front = false">
+        <div class="arr-right"></div>
+      </div>
+    </template>
+    <template v-else>
+      <div class="arrow-div" v-on:click="show_front = true">
+        <div class="arr-left"></div>
+      </div>
+      <div class="picture">
+        <img :src="pictures.original_side_profile" alt="NA" />
+      </div>
+      <div class="picture">
+        <img :src="pictures.current_side_profile" alt="NA" />
+      </div>
+    </template>
+  </div>
 </template>
 
 <script>
+export default {
+  name: "PictureSlider",
+  props: ["pictures"],
 
-export default{
-    name: "PictureSlider",
-    props: ['pictures', ],
-
-    data() {
-        return {
-            show_front: true,
-        };
-
-    },
-
-}
+  data() {
+    return {
+      show_front: true
+    };
+  }
+};
 </script>
 
 <style lang="scss">
-.picture-slider{
-    margin-top: 30px;
-    display: flex;
-    .picture{
-        height: 350px;
-        width: 47%;
+.picture-slider {
+  margin-top: 30px;
+  display: flex;
+  .picture {
+    height: 350px;
+    width: 47%;
 
-        img {
-
-            height: 350px;
-
-        }
+    img {
+      height: 350px;
     }
+  }
 
-    .arrow-div {
-        margin-left: auto;
-        width: 6%;
-        display: flex;
+  .arrow-div {
+    margin-left: auto;
+    width: 6%;
+    display: flex;
     align-items: center;
-}
-
-
+  }
 }
 </style>
