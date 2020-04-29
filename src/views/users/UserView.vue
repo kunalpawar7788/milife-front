21fe4f31e2effd44c398f1b8298af21b0ba39b53
Anuvrat Parashar <bhanuvrat@gmail.com>
2020-04-29 10:40:31 +0530

    pass correct user to router-view from userview.vue
    Update condition so that api call to fetch user details is fired only if the
    user-id in the url changes.

--- src/views/users/UserView.vue
+++ src/views/users/UserView.vue
@@ -1,7 +1,12 @@
 <template>
-  <div v-if="status=='success'">
-
-<router-view v-bind:fobj_user="fobj_user"> </router-view>
+<div v-if="status=='success'">
+  <router-view v-bind:fobj_user="fobj_user"> </router-view>
+</div>
+<div v-else-if="status=='error'" class="fc-white">
+  Oops, looks like we encountered an error, please reload the page
+</div>
+<div v-else class="fc-white">
+  {{status}}
 </div>
 </template>
 
@@ -12,48 +17,25 @@ export default {
         return {
             status: null,
             pk:  this.$route.params.pk,
-            user: {},
         }
     },
-    methods: {
-        fetch_user: function(pk){
-            this.status=null;
-            const url = process.env.VUE_APP_BASE_URL+'/api/users/' + this.pk;
-            this.$http.defaults.headers.common['Authorization'] = "Token " + localStorage.getItem('token');
-
-            return new Promise((resolve, reject) => {
-                this.$http({url: url, params:this.params, method: 'GET'})
-                    .then(resp => {
-                        this.status='success';
-                        this.user = resp.data;
-                        resolve(resp.data);
-                    }).catch(err => {
-                        reject(err);
-                    });
-            });
-        },
-    },
-
     computed: {
         fobj_user: function() {
             return Object.assign({},this.$store.state.user.user);
+        },
+    },
 
-        },
-    },
-    asyncComputed: {
-        user1:{
-            get () {
-                return  this.fetch_user();
-            },
-            default: {}
-        },
-    },
     mounted() {
-        this.$store.dispatch("user/fetch_user", this.pk);
+        if(this.$store.state.user.user.id != this.pk) {
+            this.status="loading"
+            this.$store.dispatch("user/fetch_user", this.pk).then(()=>{
+                this.status="success";
+            }).catch(()=>{
+                this.status="error";
+            });
+        }
+
         this.$store.dispatch("theme/set_theme_blue");
-    },
-    created() {
-        this.fetch_user();
     },
 }
 </script>
