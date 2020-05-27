d50beb5cf0dd2174f3a5aa0d638429b049061266
pb90453 <prabhash002.vigyaa@gmail.com>
2020-05-27 13:49:29 +0530

    upload csv styling fixed
    Styles in UploadCSVView.vue fixed.
    
    Styles in UploadCSVView.vue fixed.

--- src/views/UploadCSVView.vue
+++ src/views/UploadCSVView.vue
@@ -1,19 +1,19 @@
 <template>
 <section class="upload-csv-container">
 
-  <p>{{csvfile}}</p>
+  <div class="uploader">
+    <p>{{csvfile}}</p>
+    <input class="csvfile" ref="csvfile" v-on:change="handleFileSelect()" value="csvfile" id="file-upload" type="file" name="fileUpload" />
 
-  <input
-    class="csvfile"
-    type="file"
-    ref="csvfile"
-    id="csvfile"
-    value="csvfile"
-    v-on:change="handleFileSelect()"
-    >  </input>
+    <label for="file-upload" id="file-drag">
+        <div id="start">
+        <div>Select a file or drag here</div>
+        <span id="file-upload-btn" class="btn btn-primary">Select CSV</span>
+        </div>
+    </label>
+  </div>
 
-
-  <div class="button" v-on:click="upload_csv"> Upload selected file </div>
+  <div class="button btn_csv-save" v-on:click="upload_csv"> Save</div>
 
   <table id="history-table">
     <thead>
@@ -23,8 +23,8 @@
     <tbody>
       <tr v-for="row in records">
         <td>{{timestamp_to_display(row.created_at)}}</td>
-        <td> <div class="button" v-on:click="patch(row.id)">
-            reprocess
+        <td> <div class="button btn_csv-save" v-on:click="patch(row.id)">
+            Reprocess
         </div> </td>
       </tr>
     </tbody>
@@ -117,10 +117,79 @@ export default {
 <style lang="scss">
 section.upload-csv-container {
     color: black;
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+    height: auto;
 
     #history-table {
         width: 100%;
+        border-spacing: 10px 1.5em;
     }
 
+    .uploader {
+      display: block;
+      clear: both;
+      margin: 30px auto;
+
+    label {
+      float: left;
+      clear: both;
+      padding: 2rem 1.5rem;
+      text-align: center;
+      background: #eee;
+      border-radius: 7px;
+      border: 3px dotted #dadada;
+      transition: all .2s ease;
+      user-select: none;
+    }
+
+    #start {
+      float: left;
+      clear: both;
+      width: 100%;
+      &.hidden {
+        display: none;
+      }
+    }
+
+    input[type="file"] {
+      display: none;
+    }
+
+    div {
+      margin: 0 0 .5rem 0;
+      color: $milife-grey;
+    }
+    .btn {
+      display: inline-block;
+      margin: 2rem .5rem 0rem .5rem;
+      clear: both;
+      font-family: inherit;
+      font-weight: 400;
+      font-size: 15pt;
+      text-decoration: none;
+      text-transform: initial;
+      border: none;
+      border-radius: 2rem;
+      outline: none;
+      padding: 0 1rem;
+      line-height: 2.5em;
+      color: #fff;
+      transition: all 0.2s ease-in-out;
+      box-sizing: border-box;
+      background: $milife-magenta;
+      border-color: $milife-magenta;
+      cursor: pointer;
+    }
+  }
+  .btn_csv-save{
+    min-width: 50%;
+    line-height: 0rem;
+    color: #fff;
+    font-weight: 300;
+  }
 }
+
 </style>
