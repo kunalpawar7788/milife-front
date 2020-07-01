32be72ae4e3f93ce30d657d8dfd9cc383a2041fe
TanyaaCJain <tanya.jain27may@gmail.com>
2020-07-01 13:39:10 +0530

    Tests vue code

--- tests/unit/components/HeightInput.spec.js
+++ tests/unit/components/HeightInput.spec.js
@@ -1,58 +1,60 @@
-import Vue from "vue";
-import { shallowMount } from "@vue/test-utils";
-import HeightInput from "@/components/HeightInput.vue";
+import Vue from 'vue';
+import { shallowMount } from '@vue/test-utils';
+import HeightInput from '@/components/HeightInput.vue';
 
-describe("HeightInput.vue", () => {
+describe('HeightInput.vue', () => {
   let wrapper = shallowMount(HeightInput, {
     propsData: {
       height_retrieved_from_User: {
         magnitude_si: 170,
-        preferred_unit: "metric",
+        preferred_unit: 'metric',
       },
     },
   });
 
-  it("initializes correctly", () => {
-    expect(wrapper.name()).toMatch("HeightInput");
-    expect(wrapper.vm.height_retrieved_from_User.preferred_unit).toMatch("metric");
+  it('initializes correctly', () => {
+    expect(wrapper.name()).toMatch('HeightInput');
+    expect(wrapper.vm.height_retrieved_from_User.preferred_unit).toMatch(
+      'metric'
+    );
     expect(wrapper.vm.height_retrieved_from_User.magnitude_si).toBe(170);
-    expect(wrapper.vm.height_options).toContain("metric");
-    expect(wrapper.vm.height_options).toContain("imperial");
+    expect(wrapper.vm.height_options).toContain('metric');
+    expect(wrapper.vm.height_options).toContain('imperial');
     expect(wrapper.vm.magnitude_si).toBe(170);
     expect(wrapper.vm.feet2cm).toBe(30.48);
     expect(wrapper.vm.inch2cm).toBe(2.54);
   });
 
-  it("rounds off values correctly", () => {
+  it('rounds off values correctly', () => {
     expect(wrapper.vm.round_off2(13.345)).toBe(13.35);
   });
 
-  it("emits results", async () => {
-    wrapper.vm.height = "metric";
+  it('emits results', async () => {
+    wrapper.vm.height = 'metric';
     wrapper.vm.magnitude_si = 170;
 
     wrapper.vm.emit_result();
     await Vue.nextTick();
 
-    expect(wrapper.emitted("input")[0][0]).toEqual({
-      preferred_unit: "metric",
+    expect(wrapper.emitted('input')[0][0]).toEqual({
+      preferred_unit: 'metric',
       magnitude_si: 170,
     });
   });
 
-  it("gets cms from feet", () => {
+  it('gets cms from feet', () => {
     expect(wrapper.vm.get_cm_from_feet(5)).toBe(152.4);
   });
 
-  it("gets cm from inches", () => {
+  it('gets cm from inches', () => {
     expect(wrapper.vm.get_cm_from_inches(5)).toBe(12.7);
   });
 
-  it("gets feet from cm", () => {
+  it('gets feet from cm', () => {
     expect(wrapper.vm.get_feet_from_cm(170)).toBe(5);
   });
 
-  it("gets inches from cm", () => {
+  it('gets inches from cm', () => {
     expect(wrapper.vm.get_inches_from_cm(170)).toBe(6);
   });
 
